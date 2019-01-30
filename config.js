9c6d755ea0137ab88e803bfd7a143c428f9cf2a3
tangxinke <tomtang@thetiger.com.cn>
2019-01-30 15:02:31 +0800

    no message

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * 配置的文件
  */
 
-var isTest = false
+var isTest = true
 var identity = 'jobhunter'   //  zhaopin  or  jobhunter
 var domain = identity === 'jobhunter' ? 'qiuzhi' : 'zhaopin'
 //var baseHost = isTest?`https://${domain}-api.lieduoduo.ziwork.com`: `https://${domain}-api.lieduoduo.zike.com` 
