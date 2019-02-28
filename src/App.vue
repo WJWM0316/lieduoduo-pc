8586c5ac958de7ad9bc584d0c8e1918b6f7ef6d7
tangxinke <tomtang@thetiger.com.cn>
2019-02-28 14:39:48 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -56,6 +56,7 @@ export default class App extends Vue {
   }
 }
 </script>
+
 <style lang="less">
 
 
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -81,7 +81,14 @@
 	   */
 
 	  mounted () {
-  		
+	  	//百度统计
+  		var _hmt = _hmt || [];
+  		(function() {
+  		  var hm = document.createElement("script");
+  		  hm.src = "https://hm.baidu.com/hm.js?f7fe68c0039c09911deef47214587f2f";
+  		  var s = document.getElementsByTagName("script")[0]; 
+  		  s.parentNode.insertBefore(hm, s);
+  		})();
 		}
 
 	   // recruiterIndex applyIndex
