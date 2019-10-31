0d8f728aadcceb9568322b0c255eb09e6d8ec32c
wuweixian <wwx@thetiger.com.cn>
2019-10-31 18:25:20 +0800

    1

--- src/views/index/components/positionList.vue
+++ src/views/index/components/positionList.vue
@@ -84,7 +84,7 @@ export default {
       if (!this.isLogin) {
         this.$router.push('/login?type=msgLogin')
       } else {
-        this.$store.commit('guideQrcodePop', { switch: true, type: 'tobIndex' })
+        this.$store.commit('guideQrcodePop', { switch: true, type: 'tocIndex' })
       }
     }
   }
