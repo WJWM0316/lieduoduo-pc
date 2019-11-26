ce31064b470ffc5c140e90e005558e599d6219e3
Hudan <hudan@yunmicloud.com>
2019-11-26 19:52:33 +0800

    合并

---
+++ src/views/common/company/components/Recruitment.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="positionType">
+        <p>职位类型：</p>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default class companyRecruitment extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
