367e40fef4b2a2e6958b5c84b6fbe7ff57583849
lizifa <lphvali@thetiger.com.cn>
2019-11-27 11:08:16 +0800

    修改个字段

--- src/store/modules/company.js
+++ src/store/modules/company.js
@@ -1,43 +1,124 @@
 import { getSearchCollect } from 'API/common'
 
 const GET_SEARCH_COLLECT = 'GET_SEARCH_COLLECT'
+const UPDATE_SEARCH_COLLECT = 'UPDATE_SEARCH_COLLECT'
+const UrlParams = new URLSearchParams(window.location.search)
 
 const state = {
-  searchCollect: {
-    employeeList: [],
-    financingList: [],
-    industryList: [],
-    areaList: [],
-    show: false
-  }
+  searchCollect: null
 }
 
 const mutations = {
-  [GET_SEARCH_COLLECT](state, data) {
-    state.searchCollect = Object.assign(data, {show: true})
+  [GET_SEARCH_COLLECT] (state, infos) {
+    if (UrlParams.get('cityNums')) {
+      let cityNumsArr = UrlParams.get('cityNums').split(',')
+      infos.area.map(v => {
+        v.checked = false
+        if (cityNumsArr.includes(String(v.areaId))) {
+          v.checked = true
+        }
+      })
+    }
+    if (UrlParams.get('industryIds')) {
+      let industryIdsArr = UrlParams.get('industryIds').split(',')
+      infos.industry.map(v => {
+        v.checked = false
+        if (industryIdsArr.includes(String(v.labelId))) {
+          v.checked = true
+        }
+      })
+    }
+    if (UrlParams.get('financingIds')) {
+      let financingIdsArr = UrlParams.get('financingIds').split(',')
+      infos.financing.map(v => {
+        v.checked = false
+        if (financingIdsArr.includes(String(v.value))) {
+          v.checked = true
+        }
+      })
+    }
+    if (UrlParams.get('employeeIds')) {
+      let employeeIdsArr = UrlParams.get('employeeIds').split(',')
+      infos.employee.map(v => {
+        v.checked = false
+        if (employeeIdsArr.includes(String(v.value))) {
+          v.checked = true
+        }
+      })
+    }
+    state.searchCollect = infos
+  },
+  [UPDATE_SEARCH_COLLECT] (state, params) {
+    let { key, item, index } = params
+  	let list = state.searchCollect[key]
+    switch (key) {
+			case 'employee':
+			  if (item.value) {
+          list[index].checked = !list[index].checked
+          if (list[0].checked) {
+            list[0].checked = false
+          }
+			  } else {
+			  	if (!list[0].checked) {
+			  	  list.map((v, i, a) => v.checked = !i ? true : false)
+			  	}
+			  }
+  			break
+  		case 'financing':
+  		  if (item.value) {
+			    list[index].checked = !list[index].checked
+          if (list[0].checked) {
+            list[0].checked = false
+          }
+			  } else {
+			  	if (!list[0].checked) {
+			  	  list.map((v, i, a) => v.checked = !i ? true : false)
+			  	}
+			  }
+  			break
+  		case 'industry':
+  		  if (params.item.labelId) {
+			    list[index].checked = !list[index].checked
+          if (list[0].checked) {
+            list[0].checked = false
+          }
+			  } else {
+			  	if (!list[0].checked) {
+			  	  list.map((v, i, a) => v.checked = !i ? true : false)
+			  	}
+			  }
+  			break
+  		case 'area':
+        list[index].checked = !list[index].checked
+  			break
+			default:
+				break
+		}
   }
 }
 
 const getters = {
-  searchCollect: searchCollect => state.searchCollect
-}
-
-const actions = {
-  getSearchCollectApi(store, options) {
-    return getSearchCollect()
-      .then(res => {
-        store.commit(GET_SEARCH_COLLECT, res.data.data)
-        return res
-      })
-      .catch(error => {
-        return Promise.reject(error.data || {})
-      })
+  filterSearchCollect: state => {
+    let data = {}
+    Object.keys(state.searchCollect).forEach(key => {
+      data[key] = state.searchCollect[key].filter(v => v.checked)
+    })
+    return data
   }
 }
 
-export default {
-  state,
-  mutations,
-  getters,
-  actions
+const actions = {
+  getSearchCollectApi (store) {
+    return getSearchCollect().then(res => {
+      store.commit(GET_SEARCH_COLLECT, res.data.data)
+      return res
+    }).catch(err => {
+      return Promise.reject(err.data.data || {})
+    })
+  },
+  updateSearchCollect (store, params) {
+    store.commit(UPDATE_SEARCH_COLLECT, params)
+  }
 }
+
+export default { state, mutations, getters, actions }
