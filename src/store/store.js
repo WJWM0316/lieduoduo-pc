6e07657cf405e94afe8507a77de458bec31743f6
wuweixian <wwx@thetiger.com.cn>
2019-11-28 14:55:27 +0800

    1

--- src/store/store.js
+++ src/store/store.js
@@ -163,15 +163,7 @@ export default new Vuex.Store({
           getUserRoleInfoApi().then(({ data }) => {
             const result = data.data || {}
             store.commit('setRoleInfos', result)
-            // 登录跳转
-            if (loginData.refresh) {
-              window.location.reload()
-              return
-            } else if (loginData.needBack) {
-              router.go(-1)
-              return
-            }
-
+            // 引导创建用户
             if (state.userIdentity === 1 && !result.isJobhunter) {
               router.replace({ path: '/createUser' })
               return
@@ -181,12 +173,21 @@ export default new Vuex.Store({
               router.replace({ path: '/register' })
               return
             }
+            
+            if (loginData.refresh) {
+              window.location.reload()
+            } else if (loginData.needBack) {
+              router.go(-1)
+            } else {
+              let userIdentity = state.userIdentity
+              userIdentity === 1 ? router.replace({ path: '/index' }) : router.replace({ path: '/candidate' })
+            }
+
             // 如果是求职者
             if (state.userIdentity === 1 && result.isJobhunter) {
               // 获取简历信息
               store.dispatch('getMyResume')
             }
-            
           })
           resolve(res)
         }).catch(e => {
