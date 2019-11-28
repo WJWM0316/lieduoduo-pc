0d484ef01c514a5e7d59854a69be4c094c1acacf
lizifa <lphvali@thetiger.com.cn>
2019-11-28 19:58:01 +0800

    修改

--- src/views/c-index/components/indexBanner3.vue
+++ src/views/c-index/components/indexBanner3.vue
@@ -1,30 +1,67 @@
 <template>
 	<div class="random-banner">
+<!-- 		<a @click="routeJump(item.vkey)" class="li-item" :class="'item' + index" v-for="(item, index) in banner" :key="item.vkey">
+			<img :src="item.src" alt="loading">
+		</a> -->
 		<a @click="routeJump('2ha5usmh')" class="li-item item0">
-			<img src="~@/assets/random_images/img0.jpg" alt="">
+			<img src="~@/assets/random_images/img0.jpg" alt="loading">
 		</a>
 		<a @click="routeJump('8sdlelbu')" class="li-item item1">
-			<img src="~@/assets/random_images/img1.jpg" alt="">
+			<img src="~@/assets/random_images/img1.jpg" alt="loading">
 		</a>
 		<a @click="routeJump('inxwzsxe')" class="li-item item2">
-			<img src="~@/assets/random_images/img2.jpg" alt="">
+			<img src="~@/assets/random_images/img2.jpg" alt="loading">
 		</a>
 		<a @click="routeJump('kkwrgd1m')" class="li-item item3">
-			<img src="~@/assets/random_images/img3.jpg" alt="">
+			<img src="~@/assets/random_images/img3.jpg" alt="loading">
 		</a>
 		<a @click="routeJump('enbqgvgx')" class="li-item item4">
-			<img src="~@/assets/random_images/img4.jpg" alt="">
+			<img src="~@/assets/random_images/img4.jpg" alt="loading">
 		</a>
 		<a @click="routeJump('4utnb5ri')" class="li-item item5">
-			<img src="~@/assets/random_images/img5.jpg" alt="">
+			<img src="~@/assets/random_images/img5.jpg" alt="loading">
 		</a>
 		<a @click="routeJump('dcrjn1kv')" class="li-item item6">
-			<img src="~@/assets/random_images/img6.jpg" alt="">
+			<img src="~@/assets/random_images/img6.jpg" alt="loading">
 		</a>
 	</div>
 </template>
 <script>
 export default {
+	data () {
+		return {
+			banner: [
+				{
+					vkey: '2ha5usmh',
+					src: '../../../assets/random_images/img0.jpg'
+				},
+				{
+					vkey: '8sdlelbu',
+					src: '../../../assets/random_images/img1.jpg'
+				},
+				{
+					vkey: 'inxwzsxe',
+					src: '../../../assets/random_images/img2.jpg'
+				},
+				{
+					vkey: 'kkwrgd1m',
+					src: '../../../assets/random_images/img3.jpg'
+				},
+				{
+					vkey: 'enbqgvgx',
+					src: '../../../assets/random_images/img4.jpg'
+				},
+				{
+					vkey: '4utnb5ri',
+					src: '../../../assets/random_images/img5.jpg'
+				},
+				{
+					vkey: 'dcrjn1kv',
+					src: '../../../assets/random_images/img6.jpg'
+				}
+			]
+		}
+	},
 	methods: {
 		routeJump (vkey) {
 			this.$router.push({
@@ -64,7 +101,7 @@ export default {
 	.item1{
 		width:252px;
 		height:206px;
-		left: calc(508px + 2px);
+		left: calc(508px + 3px);
 		top: 0;
 	}
 	.item2 {
@@ -79,7 +116,7 @@ export default {
 		width:252px;
 		height:108px;
 		top: 206px;
-		left: calc(508px + 2px);
+		left: calc(508px + 3px);
 		border-top: 3px solid white;
 		border-bottom: 3px solid white;
 	}
@@ -99,7 +136,7 @@ export default {
 		width:252px;
 		height:102px;
 		top: calc(108px + 206px);
-		right: 0;
+		left: calc(252px + 3px + 253px + 3px);
 	}
 }
 </style>
