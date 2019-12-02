0c188893e8f529171a7ad8b8bc352f4cb1467da7
wuweixian <wwx@thetiger.com.cn>
2019-12-02 12:22:53 +0800

    1

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -46,7 +46,7 @@ export default class login extends Vue {
   height: 100vh;
   box-sizing: border-box;
   width: 100%;
-  background: #652791 url(#{$image-cdn-url}/images/bg_loginpage.png) no-repeat;
+  background: #00C4CD url(#{$image-cdn-url}/images/bg_loginpage.png) no-repeat;
   background-size: 100% auto;
   background-position: center;
   display: flex;
