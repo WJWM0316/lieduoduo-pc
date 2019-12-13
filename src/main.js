845bd4040d521a7852d09a19d95d50a3563a0c5d
archer <amaohe@thetiger.com.cn>
2019-12-13 14:46:43 +0800

    fix 上传图片组件兼容ie11

--- src/main.js
+++ src/main.js
@@ -2,16 +2,34 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/store'
-// import _ from 'lodash'
 import 'normalize.css'
 // 引入vue实例扩展函数库
 import './util'
 import './eleui'
 import './filters'
+
 Vue.config.productionTip = false
 Vue.prototype.$cdnPath = process.env.VUE_APP_CDN_PATH
-// Vue.prototype._ = _
-// 兼容
+
+// 兼容toBlob
+if (!HTMLCanvasElement.prototype.toBlob) {
+  Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+    value: function (callback, type, quality) {
+      var canvas = this
+      setTimeout(function () {
+        var binStr = atob(canvas.toDataURL(type, quality).split(',')[1])
+        var len = binStr.length; var arr = new Uint8Array(len)
+
+        for (var i = 0; i < len; i++) {
+          arr[i] = binStr.charCodeAt(i)
+        }
+
+        callback(new Blob([arr], { type: type || 'image/png' }))
+      })
+    }
+  })
+}
+// 兼容requestAnimFrame语法
 window.requestAnimFrame = (function () {
   return window.requestAnimationFrame ||
   window.webkitRequestAnimationFrame ||
@@ -20,6 +38,8 @@ window.requestAnimFrame = (function () {
     window.setTimeout(callback, 1000 / 60)
   }
 })()
+
+
 new Vue({
   router,
   store,
