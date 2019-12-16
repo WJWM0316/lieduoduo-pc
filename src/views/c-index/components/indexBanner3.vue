4431bd7a516265472b7b20fffd2ba9edc66e2a6d
archer <amaohe@thetiger.com.cn>
2019-12-16 10:29:30 +0800

    fix index a

--- src/views/c-index/components/indexBanner3.vue
+++ src/views/c-index/components/indexBanner3.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="random-banner">
-		<router-link tag="a" target="_blank" href="https://h5.lieduoduo.com/delicate" class="li-item item0" data-com="北京旷视科技有限公司">
+		<a  target="_blank" href="https://h5.lieduoduo.com/delicate" class="li-item item0" data-com="北京旷视科技有限公司">
 			<img src="~@/assets/random_images/img0.png" alt="loading">
-		</router-link>
+		</a>
 		<router-link target="_blank" :to="{name: 'companyDetail', query: { vkey: 'kkwrgd1m' }}" class="li-item item1" data-com="广州欢聚时代信息科技有限公司">
 			<img src="~@/assets/random_images/img1.jpg" alt="loading">
 		</router-link>
