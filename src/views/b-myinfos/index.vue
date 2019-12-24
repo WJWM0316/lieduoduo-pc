02c9c252d21099873ad18f60646ecda295ad7742
archer <amaohe@thetiger.com.cn>
2019-12-24 10:29:11 +0800

    feat 我的资料

--- src/views/b-myinfos/index.vue
+++ src/views/b-myinfos/index.vue
@@ -1,4 +1,25 @@
 <template>
   <!-- b端我的资料 -->
-  <div>123</div>
+  <div class="app-infos">
+    <div class="infos-header">
+      <h1>我的资料</h1>
+      <el-button type="primary" icon="iconfont icon-fenxiang"> 分享主页</el-button>
+    </div>
+    <p>加"<em>*</em>"号的内容，是必须填写的项目；姓名和性别为招聘官认证信息，修改请联系 400-065-5788</p>
+  </div>
 </template>
+<style lang="scss" scoped>
+.app-infos {
+  background: #fff;
+  padding: 48px 0 100px 80px !important;
+}
+.infos-header {
+  display: flex;
+  h1 {
+    flex: 1;
+    line-height: 1;
+    margin: 0;
+    color: $title-color-1;
+  }
+}
+</style>
