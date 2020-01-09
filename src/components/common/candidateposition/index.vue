0177762266b44e544ee07097c3360be5b009f4be
lyuquan <1831615701@qq.com>
2020-01-09 19:38:53 +0800

    候选人库封装

---
+++ src/components/common/candidateposition/index.vue
@@ -0,0 +1,343 @@
+<template>
+  <!-- 选择不合适原因 -->
+  <div class="interview">
+    <el-dialog
+      width="432px"
+      custom-class="app-dialog"
+      append-to-body
+      @close="handleClose"
+      :visible.sync="dialogStatus">
+      <div slot="title" v-if="type !== 'noJob'">{{detailtitle}}</div>
+      <div class="noJobBox" v-if="type === 'noJob'">
+        <div class="nullimg"><img src="@/assets/images/nullCandidate.png" /></div>
+        <div class="noJobText">
+          <p>邀请候选人面试</p>
+          <p>至少需要一个招聘中职位~</p>
+          </div>
+        <div class="noJobBtn" @click.stop="toAddJob">去发布/开放职位</div>
+      </div>
+      <div class="selectposition" v-else>
+          <div class="selectitem" v-for="(item, i) in positionLists" :key="i" @click="selectposition(item)">
+            <div class="position">
+              <div class="close" v-show="item.isOnline === 2">关闭</div>
+              <div :class="['name', item.isOnline === 2 ? 'hui' : '']">{{item.positionName}}</div>
+              <div :class="['money', item.isOnline === 2 ? 'hui' : '']">{{item.emolumentMin}}K~{{item.emolumentMax}}K</div>
+            </div>
+            <div class="info">
+              <div :class="['address', item.isOnline === 2 ? 'hui' : '']">{{item.city}}{{item.district}}</div>
+              <div :class="['year', item.isOnline === 2 ? 'hui' : '']">{{item.workExperienceName}}</div>
+              <div :class="['benke', item.isOnline === 2 ? 'hui' : '']">{{item.educationName}}</div>
+            </div>
+            <div class="selectcur" v-if="item.isOnline === 1">
+              <i :class="['iconfont icon-chenggong position bg']" v-if="item.cur"></i>
+                <i :class="['iconfont icon-beixuanxiang position']" v-else></i>
+            </div>
+            <div class="selectcur" v-else>
+              <div class="circel"></div>
+            </div>
+          </div>
+        </div>
+          <div slot="footer">
+            <div v-if="type !== 'noJob'">
+          <el-button type="default" size="small" @click="handleClose">取消</el-button>
+          <el-button type="primary" size="small" @click="handleSave">保存</el-button>
+          </div>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+<script>
+import {
+  recruiterPositonList, sureOpenupAPi } from 'API/candidateType'
+export default {
+  watch: {
+    visible (value) {
+      if (value) {
+        this.dialogStatus = true
+        this.getlist()
+      } else {
+        this.dialogStatus = false
+      }
+    }
+  },
+  props: {
+    // uid
+    jobuid: {
+      type: [String, Number],
+      required: true
+    },
+    visible: {
+      type: Boolean
+    }
+  },
+  data () {
+    return {
+      dialogStatus: false,
+      positionLists: [],
+      jobpositionid: '',
+      type: 'noJob',
+      detailtitle: '选择职位'
+    }
+  },
+  methods: {
+    handleSave () {
+      // 点击选择职位后开撩
+      if (this.type === 'selectposition') {
+        let arr = []
+        this.positionLists.map((v, k) => {
+          if (v.cur) {
+            arr.push(v)
+          }
+        })
+        if (arr.length > 0) {
+          let data = { jobhunterUid: this.jobuid, positionId: this.jobpositionid }
+          sureOpenupAPi(data).then((res) => {
+            this.$emit('update:visible', false)
+            this.$message.success('开撩成功')
+          })
+        } else {
+          this.$message.warning('请选择职位')
+        }
+      }
+    },
+    // 选择职位
+    selectposition (item) {
+      if (item.isOnline === 2) {
+        return false
+      } else {
+        this.jobpositionid = item.id
+        this.positionLists.map((v, k) => {
+          v.cur = item === v
+        })
+      }
+    },
+    getlist () {
+      let datalist = { is_online: 1 }
+      recruiterPositonList(datalist).then((res) => {
+        let arr = res.data.data
+        let hasOnline = []
+        arr.map((v, k) => {
+          v.cur = false
+          if (v.isOnline === 2) {
+            hasOnline.push(v)
+          }
+        })
+        if (arr.length === 0 || hasOnline.length === arr.length) {
+          this.type = 'noJob'
+        } else {
+          this.type = 'selectposition'
+          this.positionLists = arr
+        }
+      })
+    },
+    toAddJob () {
+      this.$router.push({
+        name: 'recruiterIndex'
+      })
+    },
+    handleClose () {
+      this.$emit('update:visible', false)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+    .nullimg{
+      width: 179px;
+      height: 98px;
+      margin: 0 auto;
+      margin-top: 32px;
+      img{
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .noJobText {
+      text-align: center;
+      color:#66666E;
+      font-size: 14px;
+      margin-top: 22px;
+    }
+    .noJobBtn {
+      width:184px;
+      height:40px;
+      background:#00C4CD;
+      border-radius:100px;
+      line-height: 40px;
+      cursor: pointer;
+      margin: 0 auto;
+      margin-top: 38px;
+      color:rgba(255,255,255,1);
+      font-size: 14px;
+      text-align: center;
+    }
+.selectposition{
+  padding: 0 40px;
+  max-height: 335px;
+overflow-y: scroll;
+&::-webkit-scrollbar {
+  width: 4px;
+  }
+  &::-webkit-scrollbar-track {
+  background:#fff;
+  -webkit-border-radius: 20px;
+  -moz-border-radius: 20px;
+  border-radius:20px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background:#DDE1E0;
+  -webkit-border-radius: 20px;
+  -moz-border-radius: 20px;
+  border-radius:20px;
+  }
+  .selectitem{
+    height:69px;
+    margin-top: 24px;
+    border-bottom:1px solid #E8E9EB;
+    position: relative;
+    .confirmbox{
+      width:220px;
+      position: absolute;
+      right: 0px;
+      bottom: -78px;
+      z-index: 999;
+      height:96px;
+      background:rgba(255,255,255,1);
+      box-shadow:0px 0px 30px 0px rgba(22,39,77,0.07);
+      .b-head{
+        height: 12px;
+        margin-top: 20px;
+        display: flex;
+        .b-img{
+          width: 12px;
+          height: 12px;
+          margin-right: 4px;
+          margin-left: 20px;
+          img{
+            width: 100%;
+            height: 100%;
+          }
+        }
+        .b-text{
+          color: #333333;
+          font-size: 12px;
+        }
+      }
+      .b-btn{
+        padding-right: 20px;
+        margin-top: 20px;
+        .think{
+          width:60px;
+          height:24px;
+          border-radius:2px;
+          color: #92929B;;
+          text-align: center;
+          margin-right: 12px;
+          float: right;
+          line-height: 24px;
+          font-size: 12px;
+          cursor: pointer;
+        }
+        .think-sure{
+          width:60px;
+          text-align: center;
+          height:24px;
+          background:#00C4CD;
+          border-radius:2px;
+          color: #fff;
+          float: right;
+          line-height: 24px;
+          cursor: pointer;
+          font-size: 12px;
+        }
+      }
+    }
+    .position{
+      height:27px;
+      width: 100%;
+      .close{
+        width:56px;
+        height:18px;
+        background:rgba(255,255,255,1);
+        border-radius:2px;
+        color:rgba(237,92,92,1);
+        font-size: 12px;
+        float: left;
+        text-align: center;
+        line-height: 18px;
+        margin-right: 10px;
+        border:1px solid rgba(237,92,92,1);
+      }
+      .name{
+        color:rgba(53,64,72,1);
+        font-weight: bold;
+        font-size: 14px;
+        float: left;
+        line-height: 20px;
+        max-width:227px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        padding-right: 18px;
+      }
+      .money{
+        float: left;
+        color:#FF9E40;
+        font-weight: bold;
+        line-height: 20px;
+        font-size: 14px;
+      }
+      .hui{
+        color:#BCBEC0;
+      }
+    }
+    .info{
+      width: 100%;
+      color: #66666E;
+      font-size:12px;
+      .address{
+        float: left;
+        background: #F4F7F7;
+        padding: 4px 6px;
+        text-align: center;
+        margin-right: 8px;
+        border-radius: 2px;
+      }
+      .year{
+        float: left;
+        background: #F4F7F7;
+        margin-right: 8px;
+        padding: 4px 6px;
+        border-radius: 2px;
+      }
+      .benke{
+        float: left;
+        background: #F4F7F7;
+        padding: 4px 6px;
+        border-radius: 2px;
+      }
+      .hui{
+        color:#BCBEC0;
+      }
+    }
+    .selectcur{
+      position: absolute;
+      width:20px;
+      height:20px;
+      color: #BCBEC0;;
+      top: 20px;
+      right: 0px;
+      .bg{
+        color: #00C4CD;
+      }
+      .circel{
+        width:14px;
+        height:14px;
+        background: #ecebeb;
+        border: 1px solid #BCBEC0;;
+        border-radius: 50%;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/common/interviewdetail/index.vue
@@ -0,0 +1,371 @@
+<template>
+  <!-- 面试安排弹窗 -->
+  <div class="interview">
+    <el-dialog
+      width="432px"
+      custom-class="app-dialog"
+      append-to-body
+      @close="handleClose"
+      :visible.sync="dialogStatus">
+      <div slot="title">{{detailtitle}}</div>
+          <!-- 面试信息 -->
+        <div class="arrangeinfo" v-if="jobhunterInfo.arrangementInfo">
+          <div class="item">
+            <div class="icon">
+              <i class="iconfont icon-mianshiguan"></i>
+            </div>
+            <div class="text">{{jobhunterInfo.arrangementInfo.realname}}</div>
+            <div class="f-line"></div>
+            <div class="phone" v-if="jobhunterInfo.arrangementInfo.mobile">{{jobhunterInfo.arrangementInfo.mobile}}</div>
+          </div>
+          <div class="item" v-if="jobhunterInfo.positionName">
+            <div class="icon">
+              <i class="iconfont icon-zhiwei1"></i>
+            </div>
+            <div class="text">{{jobhunterInfo.positionName}}</div>
+          </div>
+          <div class="item" v-if="jobhunterInfo.address">
+            <div class="icon">
+              <i class="iconfont icon-didian"></i>
+            </div>
+            <div class="text">{{jobhunterInfo.address}}{{jobhunterInfo.doorplate}}</div>
+          </div>
+            <div class="item" v-show="jobhunterInfo.status >= 51">
+              <div class="icon">
+                <i class="iconfont icon-shijian"></i>
+              </div>
+              <div class="text">{{jobhunterInfo.arrangementInfo.appointment}}</div>
+            </div>
+
+          <div v-if="jobhunterInfo.status === 12">
+          <div class="arrangeinfo-img">
+            <img src="@/assets/images/timeinfo.png" />
+          </div>
+          <p class="wait">等待候选人接受邀请</p>
+          <p class="wait_time">候选人将收到你的面试邀请，请耐心等待</p>
+          </div>
+          <div v-show="jobhunterInfo.status === 41">
+            <div class="detail">
+              <div class="detailtitle">面试时间已确认</div>
+              <div class="desc">时间已添加到面试日程，准备迎接候选人吧～</div>
+              <div class="time">{{jobhunterInfo.arrangementInfo.appointment}}</div>
+            </div>
+          </div>
+
+            <div class="detail" v-show="jobhunterInfo.status >= 51">
+              <div class="end-time">面试结果</div>
+               <!-- 到场 -->
+               <div class="stepevaluate clearfix" v-show="jobhunterInfo.status === 51">
+                <div class="titlel">
+                  <div class="dian"></div>
+                  <div class="tex">候选人到场了吗？~</div>
+                </div>
+                 <div class="stepall">
+                <div class="step" @click="hascallback('no')">
+                  <div class="icon"><img src="@/assets/images/ku.png" /></div>
+                  <div class="text">候选人没来</div>
+                </div>
+                <div class="step" @click="hascallback('yes')">
+                  <div class="icon"><img src="@/assets/images/xiao.png" /></div>
+                  <div class="text">已到场面试</div>
+                </div>
+                </div>
+              </div>
+              <!-- 评价 -->
+              <div class="stepevaluate clearfix" v-show="[57, 58].includes(jobhunterInfo.status)">
+                <div class="titlel">
+                  <div class="dian"></div>
+                  <div class="tex">到场面试后感觉如何？评价一下吧~</div>
+                </div>
+                <div class="stepall">
+                <div class="step" @click="selereson()">
+                  <div class="icon"><img src="@/assets/images/ku.png" /></div>
+                  <div class="text">不合适</div>
+                </div>
+                <div class="step" @click="haslike()">
+                  <div class="icon"><img src="@/assets/images/xiao.png" /></div>
+                  <div class="text">感觉不错</div>
+                </div>
+                </div>
+              </div>
+              <div class="end-status" v-show="[59, 60, 61].includes(jobhunterInfo.status)">
+                <div class="status-icon">
+                  <img v-show="jobhunterInfo.status === 60" src="@/assets/images/xiao.png" />
+                  <img v-show="jobhunterInfo.status === 61" src="@/assets/images/ku.png" />
+                  <img v-show="jobhunterInfo.status === 59" src="@/assets/images/ku.png" />
+                </div>
+                <div class="status-text" v-show="jobhunterInfo.status === 60">
+                  <p class="p1">已到场面试，感觉不错</p>
+                  <p class="p2">请联系候选人确认后续环节</p>
+                </div>
+                <div class="status-text2" v-show="jobhunterInfo.status === 61">
+                  <p class="p1">已到场面试，但不合适</p>
+                </div>
+                <div class="status-text2" v-show="jobhunterInfo.status === 59">
+                  <p class="p1">候选人没来</p>
+                </div>
+              </div>
+              </div>
+        </div>
+          <div slot="footer">
+      </div>
+    </el-dialog>
+  </div>
+</template>
+<script>
+import {
+  watchInvitationAPi, setAttendApi, setCommentApi } from 'API/candidateType'
+export default {
+  watch: {
+    visible (value) {
+      if (value) {
+        this.dialogStatus = true
+        this.getdetail()
+      } else {
+        this.dialogStatus = false
+      }
+    }
+  },
+  props: {
+    // 面试id
+    interviewId: {
+      type: [String, Number],
+      required: true
+    },
+    visible: {
+      type: Boolean
+    }
+  },
+  data () {
+    return {
+      dialogStatus: false,
+      jobhunterInfo: {},
+      detailtitle: '面试信息'
+    }
+  },
+  methods: {
+    getdetail () {
+      let data = { interviewId: this.interviewId }
+      watchInvitationAPi(data).then((res) => {
+        this.jobhunterInfo = res.data.data
+      })
+    },
+    // 设置是否到场
+    hascallback (type) {
+      if (type === 'no') {
+        let data1 = { interviewId: this.jobhunterInfo.interviewId, isAttend: 0 }
+        setAttendApi(data1).then((res) => {
+          this.getdetail()
+        })
+      }
+      if (type === 'yes') {
+        let data2 = { interviewId: this.jobhunterInfo.interviewId, isAttend: 1 }
+        setAttendApi(data2).then((res) => {
+          this.getdetail()
+        })
+      }
+    },
+    // 点击感觉不错
+    haslike () {
+      setCommentApi({ interviewId: this.jobhunterInfo.interviewId }).then((res) => {
+        this.getdetail()
+      })
+    },
+    handleClose () {
+      this.$emit('update:visible', false)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+      .arrangeinfo{
+        padding: 0 40px;
+        margin-top: 4px;
+        .item{
+          line-height: 22px;
+          color:#333333;
+          font-size:14px;
+          display: flex;
+          margin-bottom: 20px;
+          .icon{
+            width: 14px;
+            height: 14px;
+            float: left;
+            i{
+              color: #BCBEC0;
+              font-size: 14px;
+            }
+          }
+          .text{
+            float: left;
+            padding-left: 18px;
+          }
+          .f-line{
+            float: left;
+            width:2px;
+            height:12px;
+            margin: 5px 7px;
+            background: #E8E9EB;
+          }
+          .phone{
+            float: left;
+            // color:#00C4CD;
+          }
+        }
+        .detail{
+          width: 100%;
+          margin-top: 36px;
+          text-align: center;
+          .detailtitle{
+            font-size:18px;
+            font-weight:bold;
+            color:#333333;
+          }
+          .desc{
+            font-weight:400;
+            color:#66666E;
+            font-size: 14px;
+            margin-top: 5px;
+          }
+          .time{
+            font-weight:500;
+            font-size:28px;
+            margin-top: 24px;
+            color:#03B3BB;
+          }
+          .end-time{
+            width: 100%;
+            height:16px;
+            font-weight: bold;
+            text-align: left;
+            font-size: 16px;
+            color:rgba(53,64,72,1);
+          }
+          .stepevaluate{
+            width: 100%;
+            margin-top: 24px;
+            .titlel{
+              width: 100%;
+              height: 22px;
+              .dian{
+                width:4px;
+                height:4px;
+                background:#66666E;
+                float:left;
+                border-radius: 50%;
+                margin-top: 5px;
+                margin-right: 5px;
+              }
+              .tex{
+                color:#66666E;
+                font-size: 14px;
+                float: left;
+                font-weight: normal;
+              }
+            }
+            .stepall{
+              width: 100%;
+              height:42px;
+              .step{
+                width:160px;
+                height:42px;
+                float: left;
+                cursor: pointer;
+                background: #F8FAFA;
+                border-radius:21px;
+                margin-right: 16px;
+                margin-top: 16px;
+                margin-bottom: 10px;
+                .icon{
+                  width:42px;
+                  height:42px;
+                  float: left;
+                  img{
+                    width: 100%;
+                    height:100%;
+                  }
+                }
+                .text{
+                  width: 70px;
+                  height: 14px;
+                  margin-left: 56px;
+                  margin-top: 13px;
+                  font-size: 14px;
+                  color: #66666E;;
+                }
+                &:hover{
+                  background: #F0F3F5;
+                }
+              }
+            }
+          }
+          .end-status{
+            width:352px;
+            height:88px;
+            margin-top: 16px;
+            background:#F8FAFA;
+            border-radius:8px;
+            margin-bottom: 10px;
+            .status-icon{
+              width: 64px;
+              height: 64px;
+              float: left;
+              margin: 12px 16px 12px 51px;
+              img{
+                width: 100%;
+                height: 100%;
+              }
+            }
+            .status-text{
+              float: left;
+              margin-top: 26px;
+              .p1{
+                color:#333333;
+                font-weight: bold;
+                font-size:16px;
+              }
+              .p2{
+                font-weight:300;
+                font-size:14px;
+                margin-top: 6px;
+                color:#92929B;
+              }
+            }
+            .status-text2{
+              margin-top: 34px;
+              float: left;
+              .p1{
+                color:#333333;
+                font-weight: bold;
+                font-size:16px;
+              }
+            }
+          }
+        }
+        .arrangeinfo-img{
+          width:128px;
+          height:120px;
+          margin: 0 auto;
+          margin-top: 36px;
+          margin-bottom: 24px;
+          img{
+            width: 100%;
+            height: 100%;
+          }
+        }
+        .wait{
+          color:rgba(53,64,72,1);
+          font-weight: bold;
+          font-size:16px;
+          margin-bottom: 4px;
+          text-align: center;
+
+        }
+        .wait_time{
+          font-size:14px;
+          color:#66666E;
+          text-align: center;
+          margin-bottom: 30px;
+        }
+      }
+</style>
