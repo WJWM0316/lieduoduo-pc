f6aadda1b1c3ed7d9e5585908f0f5f96e03c04fd
diviner <48906160+diviner2@users.noreply.github.com>
2020-02-13 19:36:21 +0800

    banner配置

--- src/views/c-index/components/indexBanner4.vue
+++ src/views/c-index/components/indexBanner4.vue
@@ -3,13 +3,16 @@
     <!-- <router-link target="_blank" :to="{name: 'dreamDisco'}" class="li-item">
       <img :src="cdnPath + 'dreamBanner.jpg'" alt="loading">
     </router-link> -->
-    <div class="random-left-banner">
+    <router-link target="_blank" :to="{name: 'marketOriginality'}" class="li-item">
+      <img :src="cdnPath + 'marketOriginality_banner.png'" alt="loading">
+    </router-link>
+    <!-- <div class="random-left-banner">
       <router-link target="_blank" :to="{name: 'companyDetail', query: { vkey: 'pfpaotwi' }}" class="li-item">
         <img src="~@/assets/random_images/img1.jpg" alt="loading">
       </router-link>
       <router-link target="_blank" :to="{name: 'companyDetail', query: { vkey: 'r5fdb5ws' }}" class="li-item">
         <img src="~@/assets/random_images/img2.jpg" alt="loading">
-      </router-link>
+      </router-link> -->
       <!-- <router-link target="_blank" :to="{name: 'companyDetail', query: { vkey: 'uiwm7efh' }}" class="li-item li-item1">
         <img src="~@/assets/random_images/1.jpg" alt="loading">
       </router-link>
@@ -23,7 +26,7 @@
       <!-- <router-link target="_blank" :to="{name: 'marketOriginality', query: { cc: 'themeMC' }}" class="li-item list-item-left">
         <img src="~@/assets/random_images/active.png" alt="loading">
       </router-link> -->
-    </div>
+    <!-- </div>
     <div class="random-right-banner">
       <router-link target="_blank" :to="{name: 'companyDetail', query: { vkey: 'r9umtuv3' }}" class="li-item">
         <img src="~@/assets/random_images/img03.jpg" alt="loading">
@@ -34,7 +37,7 @@
       <router-link target="_blank" :to="{name: 'companyDetail', query: { vkey: 'no1pzts7' }}" class="li-item">
         <img src="~@/assets/random_images/img05.jpg" alt="loading">
       </router-link>
-    </div>
+    </div> -->
 	</div>
 </template>
 <script>
