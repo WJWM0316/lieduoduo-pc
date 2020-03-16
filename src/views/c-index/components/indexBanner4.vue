bd967f73c5429cedf0f976204c0e720975bb2466
diviner <48906160+diviner2@users.noreply.github.com>
2020-03-16 11:19:08 +0800

    替换banner

--- src/views/c-index/components/indexBanner4.vue
+++ src/views/c-index/components/indexBanner4.vue
@@ -6,22 +6,22 @@
     <!-- <router-link target="_blank" :to="{name: 'marketOriginality'}" class="li-item">
       <img :src="cdnPath + 'marketOriginality_banner.png'" alt="loading">
     </router-link> -->
-    <router-link target="_blank" :to="{name: 'queen', query: { sourceType: 'hd_queen_web' }}" class="li-item">
+    <!-- <router-link target="_blank" :to="{name: 'queen', query: { sourceType: 'hd_queen_web' }}" class="li-item">
       <img src="@/assets/images/activity/queen/queen_banner.png" alt="loading">
-    </router-link>
+    </router-link> -->
     <!-- <router-link target="_blank" :to="{name: 'alliance', query: { sourceType: 'hd_alliance_web' }}" class="left-banner">
       <img src="@/assets/images/activity/alliance/small_banner.png" alt="loading">
     </router-link>
     <router-link target="_blank" :to="{name: 'disease', query: { sourceType: 'hd_job_web' }}" class="right-banner">
       <img src="@/assets/images/activity/disease/diseaseBanner.png" alt="loading">
     </router-link> -->
-    <!-- <div class="random-left-banner">
+    <div class="random-left-banner">
       <router-link target="_blank" :to="{name: 'companyDetail', query: { vkey: 'pfpaotwi' }}" class="li-item">
         <img src="~@/assets/random_images/img1.jpg" alt="loading">
       </router-link>
       <router-link target="_blank" :to="{name: 'companyDetail', query: { vkey: 'r5fdb5ws' }}" class="li-item">
         <img src="~@/assets/random_images/img2.jpg" alt="loading">
-      </router-link> -->
+      </router-link>
       <!-- <router-link target="_blank" :to="{name: 'companyDetail', query: { vkey: 'uiwm7efh' }}" class="li-item li-item1">
         <img src="~@/assets/random_images/1.jpg" alt="loading">
       </router-link>
@@ -35,7 +35,7 @@
       <!-- <router-link target="_blank" :to="{name: 'marketOriginality', query: { cc: 'themeMC' }}" class="li-item list-item-left">
         <img src="~@/assets/random_images/active.png" alt="loading">
       </router-link> -->
-    <!-- </div>
+    </div>
     <div class="random-right-banner">
       <router-link target="_blank" :to="{name: 'companyDetail', query: { vkey: 'r9umtuv3' }}" class="li-item">
         <img src="~@/assets/random_images/img03.jpg" alt="loading">
@@ -46,7 +46,7 @@
       <router-link target="_blank" :to="{name: 'companyDetail', query: { vkey: 'no1pzts7' }}" class="li-item">
         <img src="~@/assets/random_images/img05.jpg" alt="loading">
       </router-link>
-    </div> -->
+    </div>
 	</div>
 </template>
 <script>
